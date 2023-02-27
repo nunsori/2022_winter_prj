e6936c9b49894c82f7006945d6aaa608f9fce111
gus6615 <wns6615@gmail.com>
2023-02-27 22:50:34 +0900

    feat: UI Object 관리용 스크립트 추가

---
+++ Assets/data/uibox_data.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uibox_data : MonoBehaviour
+{
+    public Image[] images;
+    public Text[] texts;
+    public Button button;
+    public int code;
+}
