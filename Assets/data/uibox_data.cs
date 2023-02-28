b543015f29f6215491adbcb32da8e4e0c35729cf
gus6615 <wns6615@gmail.com>
2023-02-28 13:44:07 +0900

    feat: 정복 관련 기능 추가
    - 정복 기능 UI 애니메이션 구현
    - 행성 선택 기능 구현

--- Assets/data/uibox_data.cs
+++ Assets/data/uibox_data.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class uibox_data : MonoBehaviour
 {
+    public RectTransform rectTransform;
     public Image[] images;
-    public Text[] texts;
+    public TMP_Text[] texts;
     public Button button;
     public int code;
 }
